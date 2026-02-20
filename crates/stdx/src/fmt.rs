54fb184fab6da962b0dfc2cfb9b50455f727fa85
- <5144598+-@users.noreply.github.com>
2026-02-20 07:27:33 +0300

    fix codegen

--- crates/stdx/src/fmt.rs
+++ crates/stdx/src/fmt.rs
@@ -6,7 +6,7 @@ pub struct Time(pub u64); // nanoseconds
 impl fmt::Display for Size {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     let [s, u] = f.width().map_or([0, 0], |w| [w, 2]);
-    let i = (self.0 | 1).ilog2() as usize / 10;
+    let i = self.0.max(1).ilog2() as usize / 10;
     let unit = ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei"][i];
     let size = self.0 as f64 / (1u64 << 10 * i) as f64;
     let prec = if i > 0 { precision(size) } else { 0 };
@@ -17,7 +17,7 @@ impl fmt::Display for Size {
 impl fmt::Display for Time {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     let [t, u] = f.width().map_or([0, 0], |w| [w, 1]);
-    let i = self.0.clamp(1, 1_000_000_000).ilog10() as usize / 3;
+    let i = self.0.max(1).ilog10().min(9) as usize / 3;
     let unit = ["n", "µ", "m", ""][i];
     let time = self.0 as f64 / [1e0, 1e3, 1e6, 1e9][i];
     let prec = if i > 0 { precision(time) } else { 0 };
